a0201b53ab0b6e6a5f3f2fd6365d2e4769a5b4a0
DKMFzF <150276575+DKMFzF@users.noreply.github.com>
2025-07-03 01:07:45 +0700

    feat: added event icon

--- src/components/Event/Event.jsx
+++ src/components/Event/Event.jsx
@@ -1,25 +1,39 @@
-import React from 'react';
+import { useRef, useEffect } from 'react';
 
 export function Event(props) {
-    const ref = React.useRef();
+  const ref = useRef();
+  const { onSize, icon } = props;
 
-    const { onSize } = props;
+  useEffect(() => {
+    const width = ref.current.offsetWidth;
+    const height = ref.current.offsetHeight;
+    if (onSize) {
+      onSize({ width, height });
+    }
+  });
 
-    React.useEffect(() => {
-        const width = ref.current.offsetWidth;
-        const height = ref.current.offsetHeight;
-        if (onSize) {
-            onSize({ width, height });
-        }
-    });
+  const iconPaths = {
+    temp: './icon_temperature.svg',
+    temp2: './icon_temperature_2.svg',
+    light: './icon_sun.svg',
+    light2: './icon_sun_2.svg',
+    schedule: './icon_scheduled.svg'
+  };
 
-    return <li ref={ref} className={'event' + (props.slim ? ' event_slim' : '')}>
-        <button className="event__button">
-            <span className={`event__icon event__icon_${props.icon}`} role="img" aria-label={props.iconLabel}></span>
-            <h4 className="event__title">{props.title}</h4>
-            {props.subtitle &&
-                <span className="event__subtitle">{props.subtitle}</span>
-            }
-        </button>
-    </li>;
+  const iconPath = iconPaths[icon] || '';
+
+  return (
+    <li ref={ref} className={'event' + (props.slim ? ' event_slim' : '')}>
+      <button className="event__button">
+        <span
+          className={`event__icon event__icon_${props.icon}`}
+          style={{ backgroundImage: `url(${iconPath})` }}
+          role="img"
+          aria-label={props.iconLabel}
+        ></span>
+        <h4 className="event__title">{props.title}</h4>
+        {props.subtitle && <span className="event__subtitle">{props.subtitle}</span>}
+      </button>
+    </li>
+  );
 }
